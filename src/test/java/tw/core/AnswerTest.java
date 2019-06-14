4953ad90475794bcb999b0ee2c09bc36ab8123fe
humplee <littleted1221@163.com>
2019-06-13 19:54:06 -0700

    pre-setting of answertest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,15 @@
 package tw.core;
 
+import org.junit.Before;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
-
+    Answer answer;
+    @Before
+    public void setAnswer(){
+        answer=Answer.createAnswer("1 3 5 6");
+    }
+    //answer presetting
 }
