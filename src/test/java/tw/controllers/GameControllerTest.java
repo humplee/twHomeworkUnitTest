2c527a2593cdc0446e5780603c26d3b5e10201e8
humplee <littleted1221@163.com>
2019-06-18 19:15:06 +0800

    initialize game controller test

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,26 @@
 package tw.controllers;
 
+import org.mockito.Mock;
+import tw.commands.InputCommand;
+import tw.core.Answer;
+import tw.core.Game;
+import tw.core.generator.AnswerGenerator;
+import tw.views.GameView;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
+    @Mock
+    private GameView mockGameView;
+    @Mock
+    private InputCommand mockCommand;
+    @Mock
+    private AnswerGenerator mockGenerator;
+    @Mock
+    private Game game;
 
+    private Answer correctAnswer;
+    private Answer errorAnswer;
+    private GameController gameController;
 }
