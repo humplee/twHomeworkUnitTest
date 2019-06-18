6aa60590de71f70f8ec6bdcce945b8a4bcb7e9f4
humplee <littleted1221@163.com>
2019-06-18 16:30:44 +0800

    game state test

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,38 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
 
 public class GameTest {
-
+    private final Answer actualAnswer = Answer.createAnswer("1 2 3 4");
+    private Game game;
+    @Before
+    public void setGame() throws OutOfRangeAnswerException {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(actualAnswer);
+        game = new Game(answerGenerator);
+    }
+    
+    @Test
+    public void Game_Test() throws OutOfRangeAnswerException {
+        game.guess(Answer.createAnswer("1 3 4 5"));
+        game.guess(Answer.createAnswer("2 5 8 0"));
+        game.guess(Answer.createAnswer("2 5 8 1"));
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        String gameState = game.checkStatus();
+        assertThat(gameState,is("success"));
+    }
 }
